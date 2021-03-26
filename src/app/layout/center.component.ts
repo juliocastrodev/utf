bfbc0209bce8ba2e1567dae0e7e4dc8ef0178bff
Julio Castro <julio.castro.dev@gmail.com>
2021-03-26 23:45:31 +0100

    basic layout of page

---
+++ src/app/layout/center.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center',
+  template: `<ng-content></ng-content>`,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 4rem;
+        padding: 2rem;
+        min-height: 100vh;
+      }
+    `,
+  ],
+})
+export class CenterComponent {}
