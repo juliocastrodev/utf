65e96e4344b7c98066cdbf8db14ea4ea2c14f184
Julio Castro <julio.castro.dev@gmail.com>
2021-03-28 15:43:46 +0200

    start to implement encoding wizard part 2

--- src/app/components/encoding-wizard/encoding-wizard.component.ts
+++ src/app/components/encoding-wizard/encoding-wizard.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import UTF8 from 'utf8';
 
 @Component({
   selector: 'app-encoding-wizard',
   templateUrl: './encoding-wizard.component.html',
-  styleUrls: ['./encoding-wizard.component.scss']
+  styleUrls: ['./encoding-wizard.component.scss'],
 })
 export class EncodingWizardComponent implements OnInit {
+  @Input() char: string;
+  encoded: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.encoded = UTF8.encode(this.char);
+    console.log(this.encoded);
+    console.log(this.encoded.length);
   }
-
 }
