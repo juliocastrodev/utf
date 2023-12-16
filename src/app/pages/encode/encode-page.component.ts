79bf35187687783070525466d74c53594d2608c9
Julio Castro <julio.castro.dev@gmail.com>
2023-12-16 02:57:31 +0100

    implement restart in encode page

--- src/app/pages/encode/encode-page.component.ts
+++ src/app/pages/encode/encode-page.component.ts
@@ -16,40 +16,46 @@ import { SectionComponent } from '../../shared/components/section/section.compon
     InputComponent,
     SectionComponent,
   ],
-  template: `<utf-fullscreen>
+  template: ` <utf-fullscreen>
     <h1 class="font-retro">Encode</h1>
 
-    <div class="mt-20 flex flex-col gap-2 items-center">
-      <h3 class="text-secondary">Introduce algo</h3>
-      <utf-input
-        class="max-w-[5rem]"
-        [(value)]="textToEncode"
-        [maxLength]="1"
-      />
-    </div>
-
-    <utf-section class="mt-20 mb-10 max-w-4xl [&_*]:text-secondary">
-      <h2>Bueno esto</h2>
+    <div class="mt-20 grow flex flex-col gap-12">
+      <div class="flex flex-col gap-2 items-center">
+        <h3 class="text-secondary">Introduce algo</h3>
+        <utf-input
+          class="max-w-[5rem]"
+          [(value)]="textToEncode"
+          [maxLength]="1"
+        />
+      </div>
 
       @if (encodedText) {
-        <h3>The encoded text is [{{ encodedText }}]</h3>
+        <utf-section class="max-w-4xl [&_*]:text-secondary">
+          <h2>Bueno esto</h2>
+
+          <h3>The encoded text is [{{ encodedText }}]</h3>
+
+          <p>
+            llorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem llorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem llorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem lorem lorem orem lorem lorem lorem lorem
+            lorem orem lorem lorem lorem lorem lorem orem lorem lorem
+          </p>
+        </utf-section>
       }
 
-      <p>
-        llorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-        llorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-        llorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-        lorem lorem lorem orem lorem lorem lorem lorem lorem orem lorem lorem
-        lorem lorem lorem orem lorem lorem
-      </p>
-    </utf-section>
+      <div class="mt-auto flex gap-5">
+        <utf-button utfNavigate="/">Back</utf-button>
 
-    <div class="mt-auto flex gap-5">
-      <utf-button utfNavigate="/">Back</utf-button>
+        @if (textToEncode && !encodedText) {
+          <utf-button (click)="encode()">Encode</utf-button>
+        }
 
-      @if (textToEncode) {
-        <utf-button (click)="encode()">Encode</utf-button>
-      }
+        @if (encodedText) {
+          <utf-button (click)="reestart()">Restart</utf-button>
+        }
+      </div>
     </div>
   </utf-fullscreen>`,
 })
@@ -59,5 +65,10 @@ export class EncodePageComponent {
 
   encode() {
     this.encodedText = 'awita'
+  }
+
+  reestart() {
+    this.textToEncode = ''
+    this.encodedText = ''
   }
 }
