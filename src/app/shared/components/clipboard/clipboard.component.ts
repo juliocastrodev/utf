cb9ac6c4d2183c9ce6d1227297d726d06f68a80d
Julio Castro <julio.castro.dev@gmail.com>
2023-12-27 02:12:04 +0100

    add unnecessary but fun states to clipboard component

--- src/app/shared/components/clipboard/clipboard.component.ts
+++ src/app/shared/components/clipboard/clipboard.component.ts
@@ -6,21 +6,55 @@ import { ButtonComponent } from '../button/button.component'
   selector: 'utf-clipboard',
   imports: [ButtonComponent],
   template: `
-    <utf-button (click)="handleClick()" [disabled]="clicked" variant="circle">
-      @if (!clicked) {
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          style="fill: currentColor;transform: ;msFilter:;"
-        >
-          <path
-            d="M19 3h-2.25a1 1 0 0 0-1-1h-7.5a1 1 0 0 0-1 1H5c-1.103 0-2 .897-2 2v15c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm0 17H5V5h2v2h10V5h2v15z"
-          ></path>
-        </svg>
-      } @else {
-        <span class="block w-6 h-6 leading-none">✔</span>
+    <utf-button
+      (click)="handleClick()"
+      [disabled]="state !== 'initial'"
+      variant="circle"
+    >
+      @switch (state) {
+        @case ('initial') {
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            style="fill: currentColor;transform: ;msFilter:;"
+          >
+            <path
+              d="M19 3h-2.25a1 1 0 0 0-1-1h-7.5a1 1 0 0 0-1 1H5c-1.103 0-2 .897-2 2v15c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm0 17H5V5h2v2h10V5h2v15z"
+            ></path>
+          </svg>
+        }
+
+        @case ('loading') {
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <style>
+              .spinner_aj0A {
+                fill: currentColor;
+                transform-origin: center;
+                animation: spinner_KYSC 0.75s infinite linear;
+              }
+              @keyframes spinner_KYSC {
+                100% {
+                  transform: rotate(360deg);
+                }
+              }
+            </style>
+            <path
+              d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+              class="spinner_aj0A"
+            />
+          </svg>
+        }
+
+        @case ('done') {
+          <span class="block w-6 h-6 leading-none">✔</span>
+        }
       }
     </utf-button>
   `,
@@ -28,14 +62,13 @@ import { ButtonComponent } from '../button/button.component'
 export class ClipboardComponent {
   @Input() copy = ''
 
-  clicked = false
+  state: 'initial' | 'loading' | 'done' = 'initial'
 
   async handleClick() {
-    this.clicked = true
-
+    this.state = 'loading'
     await this.copyToClipboard()
-
-    setTimeout(() => (this.clicked = false), 300)
+    setTimeout(() => (this.state = 'done'), 300)
+    setTimeout(() => (this.state = 'initial'), 600)
   }
 
   private async copyToClipboard() {
